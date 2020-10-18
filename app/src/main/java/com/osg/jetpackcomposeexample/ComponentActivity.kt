58622e6c3e729ff765a02452d99947008da54c9f
leehyeonseong <goodsnwjr2@naver.com>
2020-10-19 08:56:04 +0900

    commit -second test

--- app/src/main/java/com/osg/jetpackcomposeexample/ComponentActivity.kt
+++ app/src/main/java/com/osg/jetpackcomposeexample/ComponentActivity.kt
@@ -11,13 +11,15 @@ import android.view.PointerIcon.load
 import android.widget.ImageView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.PointerIconCompat.load
 import androidx.ui.tooling.preview.Preview
 import com.bumptech.glide.Glide
@@ -47,20 +49,22 @@ fun secondComponent(){
 
         TopAppBar(title = {Text(text="coordinate layout을 사용해 보자\n여기는 coordinate layout이 될 예정임")})
 
-        val datas=ArrayList<Test>()
-        datas.add(Test("피아노 연습책", "피아노", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
-        datas.add(Test("ㅁㅁ 연습책", "ㅁㅁ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
-        datas.add(Test("ㄴㄴ 연습책", "ㄴㄴ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
-        datas.add(Test("ㅇㅇ 연습책", "ㅇㅇ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
-        datas.add(Test("ㄹㄹ 연습책", "ㄹㄹ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
-        datas.add(Test("ㅎㅎ 연습책", "ㅎㅎ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
+        val datas=ArrayList<ClassElement>()
+
+//        datas.add(Test("피아노 연습책", "피아노", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
+//        datas.add(Test("ㅁㅁ 연습책", "ㅁㅁ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
+//        datas.add(Test("ㄴㄴ 연습책", "ㄴㄴ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
+//        datas.add(Test("ㅇㅇ 연습책", "ㅇㅇ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
+//        datas.add(Test("ㄹㄹ 연습책", "ㄹㄹ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
+//        datas.add(Test("ㅎㅎ 연습책", "ㅎㅎ", 10000, "https://madi-1302397712.cos.ap-seoul.myqcloud.com/images/5f027fbcc9986921824aae6f_1602646108078.jpg"))
+
         LazyColumnFor(datas) {item->
             Row() {
+                    imageComponent()
                 Column {
                     Text("${item.title}")
                     Text("${item.category}")
                     Text("${item.price} 원")
-                    imageComponent()
                 }
             }
         }
@@ -70,10 +74,13 @@ fun secondComponent(){
 @Composable
 fun imageComponent(){
     val image= imageResource(id = R.drawable.header)
-    Image(image)
+    val imageModifier= Modifier
+        .preferredHeight(80.dp)
+        .preferredWidth(100.dp)
+    Image(image, imageModifier)
 }
 
-//@Composable
+@Composable
 fun callData(){
     val retrofit=RetrofitHelper().getRetrofit()
     val service=retrofit.create(RetrofitService::class.java)
